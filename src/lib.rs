8bc751352b62dd56bf3f2ad307b32cd66023cf67
tormol <t.b.moltu@lyse.net>
2016-03-27 19:37:51 +0200

    Use codepoints instead of length in bytes.
    Using len() would pad non-ASCII strings like "blübb" too little (visually),
    and ASCII strings padded with non-ASCII would end up with a too high `.len()`
    
    Still doesn't handle codepoints that visually look like several,
    or those that combine with thouse around them.

--- src/lib.rs
+++ src/lib.rs
@@ -21,10 +21,12 @@ assert_eq!(leftpad(s, 7), "  blubb");
 use std::borrow::Cow;
 use std::borrow::Borrow;
 
-/// Pads a string to the given length `len` by inserting the character `pad_char` from the left.
+/// Pads a string to the given number of chars by inserting the character `pad_char` from the left.
 ///
-/// If the given string has a length longer or equal to the desired length, it will be
+/// If the given string has more than or exactly the desired number of codepoints, it will be
 /// returned as-is.
+///
+/// Codepoints are not graphemes, so the result might always not be what a human would expect.
 ///
 /// # Examples
 ///
@@ -37,16 +39,22 @@ use std::borrow::Borrow;
 /// assert_eq!(leftpad_with("blubb", 5, ' '), "blubb");
 /// assert_eq!(leftpad_with("blubb", 3, ' '), "blubb");
 /// ```
-pub fn leftpad_with<'a, S>(string: S, len: usize, pad_char: char) -> Cow<'a, str>
+pub fn leftpad_with<'a, S>(string: S, codepoints: usize, pad_char: char) -> Cow<'a, str>
     where S: Into<Cow<'a, str>>
 {
     let cow = string.into();
 
-    if !(len > cow.len()) {
+    let cow_codepoints = {
+        let s: &str = cow.borrow();
+        s.len() - s.bytes()
+                   .filter(|b| (b >> 6) == 0b10u8 )
+                   .count()
+    };
+    if codepoints <= cow_codepoints {
         return cow;
     }
 
-    let to_pad = len - cow.len();
+    let to_pad = codepoints - cow_codepoints;
     let mut padded = String::with_capacity(cow.len() + to_pad);
 
     for _ in 0..to_pad {
@@ -58,12 +66,14 @@ pub fn leftpad_with<'a, S>(string: S, len: usize, pad_char: char) -> Cow<'a, str
     padded.into()
 }
 
-/// Pads a string to the given length `len` by inserting whitespaces from the left.
+/// Pads a string to the given number of chars by inserting spaces from the left.
 ///
-/// If the given string has a length longer or equal to the desired length, it will be
+/// If the given string has more than or exactly the desired number of codepoints, it will be
 /// returned as-is.
 ///
-/// This function is equal to calling `leftpad_with(string, len, ' ')`.
+/// Codepoints are not graphemes, so the result might not always be what a human would expect.
+///
+/// This function is equal to calling `leftpad_with(string, codepoints, ' ')`.
 ///
 /// # Examples
 ///
@@ -77,8 +87,8 @@ pub fn leftpad_with<'a, S>(string: S, len: usize, pad_char: char) -> Cow<'a, str
 ///
 /// assert_eq!(leftpad("blubb", 7), leftpad_with("blubb", 7, ' '));
 /// ```
-pub fn leftpad<'a, S>(string: S, len: usize) -> Cow<'a, str>
+pub fn leftpad<'a, S>(string: S, codepoints: usize) -> Cow<'a, str>
     where S: Into<Cow<'a, str>>
 {
-    leftpad_with(string, len, ' ')
+    leftpad_with(string, codepoints, ' ')
 }
